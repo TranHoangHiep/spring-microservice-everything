9f2774d290647f93f4a457d49ab96a080962bf47
Hoang Hiep <hoanghiep@Hoangs-Mac-mini.local>
2025-04-28 22:46:43 +0700

    Adding dto class

---
+++ accounts/src/main/java/com/hoanghiep/accounts/dto/CustomersDto.java
@@ -0,0 +1,11 @@
+package com.hoanghiep.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomersDto {
+
+    private String name;
+    private String email;
+    private String mobileNumber;
+}
