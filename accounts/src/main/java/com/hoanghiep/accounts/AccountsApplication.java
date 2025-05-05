f93205e4835e5efec5d71c7d8b4ca7678dd8c130
Hoang Hiep <hoanghiep@Hoangs-Mac-mini.local>
2025-05-05 22:39:43 +0700

    Add main documentation for swagger

--- accounts/src/main/java/com/hoanghiep/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/hoanghiep/accounts/AccountsApplication.java
@@ -1,11 +1,35 @@
 package com.hoanghiep.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts Microservice REST API Documentation",
+				description = "Accounts Microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Hoang Hiep",
+						email = "hoanghiep0901@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Accounts Microservice Documentation",
+				url = "https://github.com/hoanghiep0901"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
